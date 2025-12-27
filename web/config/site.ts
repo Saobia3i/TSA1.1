f41b5d2e0cd8a1096d999185aea873ebe47cb5f7
Saobia Tinni <islamsaobia@gmail.com>
2025-12-27 15:37:50 +0600

    added links in site.ts

--- web/config/site.ts
+++ web/config/site.ts
@@ -1,12 +1,12 @@
 export const siteConfig = {
   name: 'Tensor Security Academy',
   description: 'Elite Cybersecurity Training & Professional Services',
-  url: 'https://tensorsecurity.com',
-  ogImage: 'https://tensorsecurity.com/og.png',
+  url: 'https://www.tensorsecurityacademy.com/',
+  ogImage: 'https://tensorsecurity.com/og-image.png',
   links: {
-    linkedin: 'https://linkedin.com/company/tensor-security',
-    instagram: 'https://instagram.com/tensorsecurity',
+    linkedin: 'https://www.linkedin.com/company/tensorsecurityacademy',
+    instagram: 'https://www.instagram.com/tensor_security_academy',
     facebook: 'https://facebook.com/tensorsecurity',
-    x: 'https://x.com/tensorsecurity',
+    x: 'https://x.com/Tensor_Security',
   },
 };
