40de2d2df374c4d4ca03ebcca1b139ae83bbc58e
Saobia Tinni <islamsaobia@gmail.com>
2026-01-09 13:44:02 +0600

    hidded course prices

--- web/features/courses/components/CourseDetailsModal.tsx
+++ web/features/courses/components/CourseDetailsModal.tsx
@@ -314,7 +314,7 @@ export default memo(function CourseDetailsModal({
                 color: "white", // Ensure text is white inside the link
               }}
             >
-              Enroll Now {course.price && `• ${course.price}`}
+              Enroll Now {/* {course.price && `• ${course.price}`} */}
             </Link>
           </motion.button>
         </div>
